5e4254e0ad36b7e8936dc49cc943f9793ec674e4
Kelvin Ao <kelvinaoao@gmail.com>
2018-10-13 00:21:54 +0800

    Problem set 2 complete

--- Problem Set 2/bmi_script.js
+++ Problem Set 2/bmi_script.js
@@ -7,15 +7,9 @@ function clear_form(form){
 
 }
 
-function bmi(weight, height) {
-
-          bmindx=weight/eval(height*height);
-          return bmindx;
-}
-
 function check_form(form) {
 
-       if (form.weight.value==null||form.weight.value.length==0 || form.height.value==null||form.height.value.length==0){
+       if (form.weight.value.length==0 || form.height.value.length==0){
             alert("Please complete the form first.");
             return false;
        }
@@ -24,7 +18,7 @@ function check_form(form) {
                 parseFloat(form.height.value) >=500||
                 parseFloat(form.weight.value) <= 0||
                 parseFloat(form.weight.value) >=500){
-                alert("\nThe values are beyond human's limit. \nPlease input your actual values.");
+                alert("The values are beyond human's limit. Please input your actual values.");
                 clear_form(form);
                 return false;
        }
@@ -36,22 +30,25 @@ function compute_form(form) {
 
        if (check_form(form)) {
 
-       bmi_value=Math.round(bmi(form.weight.value, form.height.value/100));
-       form.bmi.value=bmi_value;
+         var weight = Number(document.getElementById("weight").value);
+         var height = Number(document.getElementById("height").value);
+         var bmi = weight / Math.pow(height,2)* 10000;
 
-       if (bmi_value >=30) {
+        form.bmi.value = parseInt(bmi)
+
+       if (form.bmi.value >=30) {
           form.comment.value="You are obese, please consult your doctor or dietitian.";
        }
 
-       else if (bmi_value >=25 && bmi_value <30) {
+       if (bmi >=25 && bmi <30) {
           form.comment.value="You are overweight, please make an attempt to keep fit.";
        }
 
-       else if (bmi_value >18 && bmi_value <25) {
+       if (bmi >18 && bmi <25) {
           form.comment.value="You are healthy!";
        }
 
-       else if (bmi_value <=18) {
+       if (bmi <=18) {
           form.comment.value="You are underweight, go and have more food.";
        }
 
